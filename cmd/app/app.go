095227e9bba484b05488643bc036d98995ededd0
keito tada <keito0tada@gmail.com>
2025-04-25 16:39:44 +0900

    middlewareの追加

--- cmd/app/app.go
+++ cmd/app/app.go
@@ -2,19 +2,36 @@ package app
 
 import (
 	"fmt"
+	"net/http"
 	"todo_back/infrastructure/config"
 	"todo_back/infrastructure/persistence"
 	"todo_back/interface/handler"
 	"todo_back/usecase"
 
-	"github.com/caarlos0/env"
+	"github.com/caarlos0/env/v11"
 	"github.com/clerk/clerk-sdk-go/v2"
+	"github.com/clerk/clerk-sdk-go/v2/user"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 type EnvVar struct {
 	clerk_sec_key string `env:"CLERK_SECRET_KEY"`
+}
+
+func authenticationMiddleware(client *user.Client) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		claims, ok := clerk.SessionClaimsFromContext(ctx.Request.Context())
+		if !ok {
+			fmt.Print("unauthorized")
+			return
+		}
+		usr, err := user.Get(ctx.Request.Context(), claims.Subject)
+		if err != nil {
+			fmt.Println("error")
+		}
+		fmt.Printf("%s %s\n", usr.ID, *usr.FirstName)
+	}
 }
 
 func Run() {
@@ -29,7 +46,9 @@ func Run() {
 	todoUseCase := usecase.NewTodoUsecase(todoPersistence)
 	todoHandler := handler.NewTodoHandler(db, todoUseCase)
 
-	clerk.SetKey(env_var.clerk_sec_key)
+	clerk_config := &clerk.ClientConfig{}
+	clerk_config.Key = &env_var.clerk_sec_key
+	clerk_client := user.NewClient(clerk_config)
 
 	router := gin.Default()
 
@@ -40,8 +59,15 @@ func Run() {
 		AllowCredentials: false,
 	}))
 
+	router.Use(authenticationMiddleware(clerk_client))
+
 	router.POST("/todos", todoHandler.HandleTodoInsert)
 	router.GET("/todos", todoHandler.HandleTodoGetAll)
+
+	router.GET("ping", func(ctx *gin.Context) {
+		fmt.Println("pong")
+		ctx.String(http.StatusOK, "pong")
+	})
 
 	router.Run("0.0.0.0:8080")
 }

79208b45d75b97d156717ea7646267f4994a5be2
keito0tada <keito0tada@gmail.com>
2025-05-02 20:03:55 +0900

    POSTGRES_URL->DATABASE_URL

--- api/infrastructure/config/config.go
+++ api/infrastructure/config/config.go
@@ -10,12 +10,7 @@ import (
 )
 
 type db_env struct {
-	Host string `env:"POSTGRES_HOSTNAME"`
-	User string `env:"POSTGRES_USER"`
-	Pass string `env:"POSTGRES_PASSWORD"`
-	Port string `env:"POSTGRES_PORT"`
-	DB   string `env:"POSTGRES_DB"`
-	DSN  string `env:"POSTGRES_URL"`
+	DSN string `env:"DATABASE_URL"`
 }
 
 func Connect(dsn string, timeout, interval time.Duration) *gorm.DB {

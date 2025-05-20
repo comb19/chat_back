0d0b46a2413e39854f21168a57477ae5d60c9e17
kasatomorning <9549356+kasatomorning@users.noreply.github.com>
2025-05-21 00:49:28 +0900

    feat: スキーマ変更 (#39)
    * feat:スキーマ変更
    
    * chore:スキーマファイルのsyntax errorを修正

--- api/domain/model/user.go
+++ api/domain/model/user.go
@@ -1,6 +1,10 @@
 package model
 
+import "time"
+
 type User struct {
-	ID       string
-	UserName string
+	ID        string
+	UserName  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
--- api/domain/model/user_guilds.go
+++ api/domain/model/user_guilds.go
@@ -1,6 +1,10 @@
 package model
 
+import "time"
+
 type UserGuilds struct {
-	UserID  string
-	GuildID string
+	UserID    string
+	GuildID   string
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
